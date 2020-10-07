7f4f40de23952f5cb5346c9c4fd2e4798381708d
Aryat <aryatsrivastava06@gmail.com>
2020-10-07 23:52:26 +0530

    Reverse Linked List Implementation

--- C#/ALlgoDataStructureProject/Program.cs
+++ C#/ALlgoDataStructureProject/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using ALlgoDataStructureProject.DataStructure.LinkedList;
 
 namespace ALlgoDataStructureProject
 {
@@ -6,7 +7,25 @@ namespace ALlgoDataStructureProject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("test");
+            LinkedList<int> ls = new LinkedList<int>();
+            ls.InsertElementToEnd(5);
+            ls.InsertElementToEnd(6);
+            ls.InsertElementToEnd(7);
+            var x = ls.ValueAt(2);
+            var y = ls.PopFirstElement();
+            Console.WriteLine("Value at index: "+x);
+            Console.WriteLine("Head Popped"+y);
+            ls.InsertElementAtStart(4);
+            ls.InsertElementToEnd(8);
+            ls.PrintElements();
+            Console.WriteLine("Last Element: "+ls.GetLastElement());
+            ls.RemoveItemWithValue(7);
+            Console.WriteLine("2nd Item from Last: "+ls.ValueNFromEnd(2));
+            ls.PrintElements();
+            ls.ReverseLinkedList();
+            ls.PrintElements();
+            Console.ReadLine();
         }
     } 
 }
