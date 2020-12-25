a5a4d2466196114186f69b05c54baea129a52ed6
Aryat <aryatsrivastava06@gmail.com>
2020-12-25 19:26:37 +0530

    STL Algorithm practice

---
+++ STLAlgorithm/activity_selection.cpp
@@ -0,0 +1,77 @@
+// You are given n activities (from 0 to n-1) with their start and finish times. Select the maximum number of activities that can be performed by a single person, assuming that a person can only work on a single activity at a time.
+
+// Input Format
+// The first line consists of an integer T, the number of test cases. For each test case, the first line consists of an integer N, the number of activities. Then the next N lines contain two integers m and n, the start and end time of each activity.
+
+// Constraints
+// 1<=t<=50
+// 1<=n<=10000
+// 1<=A[i]<=100
+
+// Output Format
+// For each test case find the maximum number of activities that you can do.
+
+// Sample Input
+// 1
+// 3
+// 10 20
+// 12 15
+// 20 30
+// Sample Output
+// 2
+// Explanation
+// Person can only do 0th and 2nd activities.
+
+#include <iostream>
+
+using namespace std;
+
+struct Activity
+{
+    int start;
+    int finish;
+};
+
+bool activityCompare(Activity s1, Activity s2)
+{
+    return (s1.finish < s2.finish);
+}
+
+void printMaxActivitiesCount(Activity arr[], int n)
+{
+    // Sort jobs according to finish time
+    sort(arr, arr + n, activityCompare);
+
+    // The first activity always gets selected
+    int i = 0;
+
+    int count = 0;
+    for (int j = 1; j < n; j++)
+    {
+        if (arr[j].start >= arr[i].finish)
+        {
+            count++;
+            i = j;
+        }
+    }
+    cout << count;
+}
+int main()
+{
+    int cases = 0;
+    cin >> cases;
+    for (int i = 0; i < cases; i++)
+    {
+        int activities = 0;
+        cin >> activities;
+        Activity arr[activities];
+        for (int i = 0; i < activities; i++)
+        {
+            cin >> arr[i].start;
+            cin >> arr[i].finish;
+        }
+        printMaxActivitiesCount(arr, activities);
+    }
+
+    return 0;
+}
--- STLAlgorithm/next_permutation_imp.cpp
+++ STLAlgorithm/next_permutation_imp.cpp
@@ -20,7 +20,7 @@
 // Sample Input
 // 2
 // 3
-// 1 2 3 
+// 1 2 3
 // 3
 // 3 2 1
 
@@ -34,11 +34,72 @@
 
 // Code Edit
 
-#include <iostream>
+//Solution Steps
+// I) Traverse the given number from rightmost digit, keep traversing till you find a digit which is smaller than the previously traversed digit. For example, if the input number is “534976”, we stop at 4 because 4 is smaller than next digit 9. If we do not find such a digit, then output is “Not Possible”.
 
+// II) Now search the right side of above found digit ‘d’ for the smallest digit greater than ‘d’. For “534976″, the right side of 4 contains “976”. The smallest digit greater than 4 is 6.
+
+// III) Swap the above found two digits, we get 536974 in above example.
+
+// IV) Now sort all digits from position next to ‘d’ to the end of number. The number that we get after sorting is the output. For above example, we sort digits in bold 536974. We get “536479” which is the next greater number for input 534976.
+
+// Following are the implementation of above approach.
+
+#include <iostream>
+#include <algorithm>
 using namespace std;
+
+void print_next_permutation(int *a, int n)
+{
+    bool perm_found = false;
+    int i = n - 2;
+    while (i >= 0)
+    {
+        if (a[i] < a[i + 1])
+        {
+            int max = a[i + 1];
+            int max_index = i + 1;
+            for (int j = i + 1; j < n; j++)
+            {
+                if (a[j] < max && a[j] > a[i])
+                {
+                    max = a[j];
+                    max_index = j;
+                }
+            }
+            swap(a[i], a[max_index]);
+            sort(a + i + 1, a + n);
+            perm_found = true;
+            break;
+        }
+        i--;
+    }
+    if (!perm_found)
+    {
+        sort(a, a + n);
+    }
+    for (int k = 0; k < n; k++)
+    {
+        cout << a[k] << " ";
+    }
+    cout << endl;
+}
 
 int main()
 {
+    int tests = 0;
+    cin >> tests;
+    for (int i = 0; i < tests; i++)
+    {
+        int arr_size = 0;
+        cin >> arr_size;
+        int arr[arr_size];
+        for (int i = 0; i < arr_size; i++)
+        {
+            cin >> arr[i];
+        }
+        print_next_permutation(arr, arr_size);
+    }
+
     return 0;
 }
